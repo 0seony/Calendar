56bbacdab02bf4ab784cb995294123ca27319972
Yeongseon Park <yseon406@gmail.com>
2022-02-04 23:57:00 +0900

    break & continue

--- src/seony/calendar/Calendar.java
+++ src/seony/calendar/Calendar.java
@@ -1,35 +1,36 @@
 package seony.calendar;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calendar {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Calendar cl = new Calendar();
-		System.out.println("반복횟수를 입력하세요.");
-		int loop = sc.nextInt();
-		System.out.println("월을 입력하세요.");
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		int i = loop;
-		while(i>0) {
-			array.add(sc.nextInt());
-			i--;
+		while (true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print("> ");
+			int month = sc.nextInt();
+			if (month == -1) {
+				System.out.println("Have a nice day!");
+				break;
+			} else if (month<1 || month>12) {
+				continue;
+			} else {
+				cl.printMaxDays(month);
+				System.out.println();
+			}
 		}
-		for(i = 0; i<loop; i++) {
-			cl.printMaxDays(array.get(i));
-			System.out.println();
-		}
+		sc.close();
 	}
-	
+
 	public void printMaxDays(int month) {
 		// 숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Calendar cl = new Calendar();
-		System.out.printf("%d월은 %d일까지 있습니다.", month, cl.maxdays(month));
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cl.calMaxDays(month));
 	}
-	
+
 	public void printSampleCalendar() {
-		//println()을 이용해 달력 만들기
+		// println()을 이용해 달력 만들기
 		System.out.println(" 일   월   화  수   목    금   토");
 		System.out.println("--------------------");
 		System.out.println("  1    2    3   4    5    6    7");
@@ -37,10 +38,11 @@ public class Calendar {
 		System.out.println(" 15  16  17  18   19  20  21");
 		System.out.println("22  23  24  25  26  27  28");
 	}
-	
-	private static final int[] MAX_DAYS	= {31,28,31,30,31,30,31,31,30,31,30,31};
-	public int maxdays(int month) {
-		return MAX_DAYS[month-1];
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int calMaxDays(int month) {
+		return MAX_DAYS[month - 1];
 	}
 
 }
